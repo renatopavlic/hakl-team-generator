42712a57c046165f31410b1b8cc3972fd09c3c60
Renato Pavlic <renato.pavlic@disxt.com>
2022-11-09 12:48:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { players } from "./const";
+import { shuffleArray } from "./helpers";
 
 function App() {
+  const [teamOne, setTeamOne] = useState([]);
+  const [teamTwo, setTeamTwo] = useState([]);
+  const [selectedPlayers, setSelectedPlayers] = useState([]);
+
+  const handleCheckboxChange = (player) => (e) => {
+    const { checked } = e.target;
+    const existingPlayer = selectedPlayers?.find((p) => p.name === player.name);
+
+    if (checked && !existingPlayer) {
+      setSelectedPlayers((old) => [...old, player]);
+    }
+
+    if (!checked && existingPlayer) {
+      setSelectedPlayers((old) => old.filter((p) => p.name !== player.name));
+    }
+  };
+
+  const generateTeams = (players) => {
+    const teamOne = [];
+    const teamTwo = [];
+
+    const groupedByTear = players.reduce((r, a) => {
+      r[a.tier] = r[a.tier] || [];
+      r[a.tier].push(a);
+      return r;
+    }, {});
+
+    Object.entries(groupedByTear).forEach(([t, p]) => {
+      shuffleArray(p);
+    });
+
+    Object.entries(groupedByTear).forEach(([group, playersArr]) => {
+      playersArr.forEach((player) => {
+        if (!teamOne.length) {
+          teamOne.push(player.name);
+          return;
+        }
+
+        if (!teamTwo.length) {
+          teamTwo.push(player.name);
+          return;
+        }
+
+        if (teamOne.length >= teamTwo.length) {
+          teamTwo.push(player.name);
+          return;
+        }
+
+        if (teamOne.length < teamTwo.length) {
+          teamOne.push(player.name);
+          return;
+        }
+      });
+    });
+
+    setTeamOne(teamOne);
+    setTeamTwo(teamTwo);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Tko dolazi</h2>
+      {players.map((player) => (
+        <div key={player.name}>
+          <label>{player.name}</label>
+          <input
+            name={player.name}
+            type="checkbox"
+            onChange={handleCheckboxChange(player)}
+          />
+        </div>
+      ))}
+      <h1>Team generator</h1>
+      <button onClick={() => generateTeams(selectedPlayers)}>
+        Generate Teams
+      </button>
+      <div
+        style={{ display: "flex", width: 500, justifyContent: "space-between" }}
+      >
+        <div>
+          {teamOne.sort().map((player) => (
+            <p key={player}>{player}</p>
+          ))}
+        </div>
+        <div>
+          {teamTwo.sort().map((player) => (
+            <p key={player}>{player}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
