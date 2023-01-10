9c38f35ab754f6beebe32aebc92daa0e57508984
Renato Pavlic <renato.pavlic@disxt.com>
2023-01-10 17:14:27 +0100

    update 1.0

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./style.css";
 function App() {
   const [teamOne, setTeamOne] = useState([]);
   const [teamTwo, setTeamTwo] = useState([]);
+  const [playersList, setPlayersList] = useState(players || []);
   const [selectedPlayers, setSelectedPlayers] = useState([]);
   const [errorMsg, setErrorMsg] = useState(null);
 
@@ -17,10 +18,12 @@ function App() {
     const existingPlayer = selectedPlayers?.find((p) => p.name === player.name);
 
     if (checked && !existingPlayer) {
+      // add player
       setSelectedPlayers((old) => [...old, player]);
     }
 
     if (!checked && existingPlayer) {
+      // delete player
       setSelectedPlayers((old) => old.filter((p) => p.name !== player.name));
     }
   };
@@ -30,16 +33,27 @@ function App() {
 
     setErrorMsg(null);
 
-    const existingPlayer = players.find(
+    const existingPlayer = playersList.find(
       (player) =>
         player.name.toLowerCase() === nameRef.current.value.toLowerCase()
     );
     if (existingPlayer) {
-      setErrorMsg("ovog vec imamo na list");
+      setErrorMsg(
+        <p style={{ color: "red", textAlign: "center" }}>
+          Ovog vec imamo na listi
+        </p>
+      );
       return;
     }
 
-    players.push({ name: nameRef.current.value, tier: tierRef.current.value });
+    setPlayersList((old) => [
+      ...old,
+      {
+        name: nameRef.current.value,
+        tier: tierRef.current.value,
+        image: "default.png",
+      },
+    ]);
   };
 
   const generateTeams = (players) => {
@@ -57,27 +71,42 @@ function App() {
     });
 
     Object.entries(groupedByTear).forEach(([group, playersArr]) => {
-      playersArr.forEach((player) => {
-        if (!teamOne.length) {
-          teamOne.push(player.name);
-          return;
-        }
+      console.log("players arr", playersArr);
+      playersArr
+        .sort((a, b) => (b.name === "reno") - (a.name === "reno"))
+        .forEach((player) => {
+          if (!teamOne.length) {
+            teamOne.push(player.name);
+            return;
+          }
 
-        if (!teamTwo.length) {
-          teamTwo.push(player.name);
-          return;
-        }
+          if (!teamTwo.length) {
+            teamTwo.push(player.name);
+            return;
+          }
 
-        if (teamOne.length >= teamTwo.length) {
-          teamTwo.push(player.name);
-          return;
-        }
+          if (player.name === "reno") {
+            if (teamOne.includes("joho")) {
+              teamOne.push(player.name);
+              return;
+            }
 
-        if (teamOne.length < teamTwo.length) {
-          teamOne.push(player.name);
-          return;
-        }
-      });
+            if (teamTwo.includes("joho")) {
+              teamTwo.push(player.name);
+              return;
+            }
+          }
+
+          if (teamOne.length > teamTwo.length) {
+            teamTwo.push(player.name);
+            return;
+          }
+
+          if (teamOne.length <= teamTwo.length) {
+            teamOne.push(player.name);
+            return;
+          }
+        });
     });
 
     setTeamOne(teamOne);
@@ -86,37 +115,65 @@ function App() {
 
   return (
     <div className="app">
-      <h1 className="main-title">Dodaj igrača</h1>
-      <form className="form-wrapper" onSubmit={handleSubmit}>
-        <input placeholder="Ime" ref={nameRef} required />
-        <input
-          type="number"
-          min="1"
-          max="4"
-          placeholder="Jakosna skupina"
-          ref={tierRef}
-          required
-        />
-        <button className="submit-btn" type="submit">
-          Dodaj
-        </button>
-      </form>
+      <div>
+        <h1 className="main-title">Dodaj igrača</h1>
+        <form className="form-wrapper" onSubmit={handleSubmit}>
+          <input placeholder="Ime" ref={nameRef} required />
+          <input
+            type="number"
+            min="1"
+            max="4"
+            placeholder="Jakosna skupina"
+            ref={tierRef}
+            required
+          />
+          <button className="submit-btn" type="submit">
+            Dodaj
+          </button>
+        </form>
+      </div>
       {errorMsg ?? null}
+      {/* List section */}
       <div>
         <h1 className="main-title">Tko dolazi</h1>
         <div className="player-list-wrapper">
-          {players.map((player) => (
-            <div key={player.name} className="checkbox-wrapper">
-              <input
-                name={player.name}
-                type="checkbox"
-                onChange={handleCheckboxChange(player)}
+          {playersList.map((player) => (
+            <div
+              key={player.name}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src={require(`./images/${player.image}`)}
+                alt="reno"
+                style={{
+                  aspectRatio: 1,
+                  height: 50,
+                  maxWidth: 50,
+                  objectFit: "cover",
+                  borderRadius: "50%",
+                  marginBottom: "0.5rem",
+                }}
               />
-              <label>{player.name}</label>
+              <div
+                className="checkbox-wrapper"
+                style={{ background: "white", padding: "0 5px" }}
+              >
+                <input
+                  name={player.name}
+                  type="checkbox"
+                  onChange={handleCheckboxChange(player)}
+                />
+                <label>{player.name}</label>
+              </div>
             </div>
           ))}
         </div>
       </div>
+      {/* Team Generator Section */}
       <div className="team-generator-wrapper">
         <h1 className="main-title">Team generator</h1>
         <button onClick={() => generateTeams(selectedPlayers)}>
